976aee64537b92e9554be28f148b13ef2b8eb36c
Bedford Peterson <bedford.peterson@onedatascan.com>
2018-08-03 11:25:27 -0400

    Added an element that is used at the entry point and also added it to a dynamic import() on View Two (my-view2.js

--- src/my-app-elements/my-app/my-app.js
+++ src/my-app-elements/my-app/my-app.js
@@ -8,21 +8,28 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-import { PolymerElement, html } from '../../../node_modules/@polymer/polymer/polymer-element.js';
-import { setPassiveTouchGestures, setRootPath } from '@polymer/polymer/lib/utils/settings.js';
-import '@polymer/app-layout/app-drawer/app-drawer.js';
-import '@polymer/app-layout/app-drawer-layout/app-drawer-layout.js';
-import '@polymer/app-layout/app-header/app-header.js';
-import '@polymer/app-layout/app-header-layout/app-header-layout.js';
-import '@polymer/app-layout/app-scroll-effects/app-scroll-effects.js';
-import '@polymer/app-layout/app-toolbar/app-toolbar.js';
-import '@polymer/app-route/app-location.js';
-import '@polymer/app-route/app-route.js';
-import '@polymer/iron-pages/iron-pages.js';
-import '@polymer/iron-selector/iron-selector.js';
-import '@polymer/paper-icon-button/paper-icon-button.js';
-import '../my-icons.js';
-import { style } from './my-app-css.js';
+import {
+  PolymerElement,
+  html
+} from "../../../node_modules/@polymer/polymer/polymer-element.js";
+import {
+  setPassiveTouchGestures,
+  setRootPath
+} from "@polymer/polymer/lib/utils/settings.js";
+import "@polymer/app-layout/app-drawer/app-drawer.js";
+import "@polymer/app-layout/app-drawer-layout/app-drawer-layout.js";
+import "@polymer/app-layout/app-header/app-header.js";
+import "@polymer/app-layout/app-header-layout/app-header-layout.js";
+import "@polymer/app-layout/app-scroll-effects/app-scroll-effects.js";
+import "@polymer/app-layout/app-toolbar/app-toolbar.js";
+import "@polymer/app-route/app-location.js";
+import "@polymer/app-route/app-route.js";
+import "@polymer/iron-pages/iron-pages.js";
+import "@polymer/iron-selector/iron-selector.js";
+import "@polymer/paper-icon-button/paper-icon-button.js";
+import "../my-icons.js";
+import { style } from "./my-app-css.js";
+import "../x-pe/x-pe.js";
 
 // Gesture events like tap and track generated from touch will not be
 // preventable, allowing for better scrolling performance.
@@ -60,7 +67,7 @@ class MyApp extends PolymerElement {
           <app-header slot="header" condenses="" reveals="" effects="waterfall">
             <app-toolbar>
               <paper-icon-button icon="my-icons:menu" drawer-toggle=""></paper-icon-button>
-              <div main-title="">My App</div>
+              <div main-title="">My App - <x-pe></x-pe></div>
             </app-toolbar>
           </app-header>
 
@@ -80,7 +87,7 @@ class MyApp extends PolymerElement {
       page: {
         type: String,
         reflectToAttribute: true,
-        observer: '_pageChanged'
+        observer: "_pageChanged"
       },
       routeData: Object,
       subroute: Object
@@ -88,9 +95,7 @@ class MyApp extends PolymerElement {
   }
 
   static get observers() {
-    return [
-      '_routePageChanged(routeData.page)'
-    ];
+    return ["_routePageChanged(routeData.page)"];
   }
 
   async testAsync() {
@@ -100,16 +105,16 @@ class MyApp extends PolymerElement {
   }
 
   _routePageChanged(page) {
-     // Show the corresponding page according to the route.
-     //
-     // If no page was found in the route data, page will be an empty string.
-     // Show 'view1' in that case. And if the page doesn't exist, show 'view404'.
+    // Show the corresponding page according to the route.
+    //
+    // If no page was found in the route data, page will be an empty string.
+    // Show 'view1' in that case. And if the page doesn't exist, show 'view404'.
     if (!page) {
-      this.page = 'view1';
-    } else if (['view1', 'view2', 'view3'].indexOf(page) !== -1) {
+      this.page = "view1";
+    } else if (["view1", "view2", "view3"].indexOf(page) !== -1) {
       this.page = page;
     } else {
-      this.page = 'view404';
+      this.page = "view404";
     }
 
     // Close a non-persistent drawer when the page & route are changed.
@@ -124,20 +129,20 @@ class MyApp extends PolymerElement {
     // Note: `polymer build` doesn't like string concatenation in the import
     // statement, so break it up.
     switch (page) {
-      case 'view1':
-        import('../my-view1/my-view1.js');
+      case "view1":
+        import("../my-view1/my-view1.js");
         break;
-      case 'view2':
-        import('../my-view2/my-view2.js');
+      case "view2":
+        import("../my-view2/my-view2.js");
         break;
-      case 'view3':
-        import('../my-view3/my-view3.js');
+      case "view3":
+        import("../my-view3/my-view3.js");
         break;
-      case 'view404':
-        import('../my-view404/my-view404.js');
+      case "view404":
+        import("../my-view404/my-view404.js");
         break;
     }
   }
 }
 
-window.customElements.define('my-app', MyApp);
+window.customElements.define("my-app", MyApp);
--- src/my-app-elements/my-view2/my-view2.js
+++ src/my-app-elements/my-view2/my-view2.js
@@ -8,8 +8,12 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-import { PolymerElement, html } from '../../../node_modules/@polymer/polymer/polymer-element.js';
-import '../shared-styles.js';
+import {
+  PolymerElement,
+  html
+} from "../../../node_modules/@polymer/polymer/polymer-element.js";
+import "../shared-styles.js";
+import "../x-pe/x-pe.js";
 
 class MyView2 extends PolymerElement {
   static get template() {
@@ -24,7 +28,7 @@ class MyView2 extends PolymerElement {
 
       <div class="card">
         <div class="circle">2</div>
-        <h1>View Two</h1>
+        <h1>View Two - <x-pe></x-pe></h1>
         <p>Ea duis bonorum nec, falli paulo aliquid ei eum.</p>
         <p>Id nam odio natum malorum, tibique copiosae expetenda mel ea.Detracto suavitate repudiandae no eum. Id adhuc minim soluta nam.Id nam odio natum malorum, tibique copiosae expetenda mel ea.</p>
       </div>
@@ -32,4 +36,4 @@ class MyView2 extends PolymerElement {
   }
 }
 
-window.customElements.define('my-view2', MyView2);
+window.customElements.define("my-view2", MyView2);
